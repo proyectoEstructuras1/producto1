fc58386dabeb7a40ba4e3e6dbc3924e2fd67a5a9
proyectoEstructuras1 <57818462+proyectoEstructuras1@users.noreply.github.com>
2019-11-16 19:48:05 -0600

    Update Producto.cpp

--- Producto.cpp
+++ Producto.cpp
@@ -1,19 +1,44 @@
 #include "Producto.h"
 
+void Producto::verifica(void)
+{
+    if (this->numero<0)
+        {
+        numero=0;
+         float p;
+    cout<<"numero invalido"<<endl;
+    cout<<"cual es el numero:";cin>> p;
+    modificaNumero(p);
+    verifica();
+        }
+    if(this->precio<0)
+    {
+    precio=0.0;
+    float p;
+    cout<<"precio invalido"<<endl;
+    cout<<"cual es el precio:";cin>> p;
+    modificaPrecio(p);
+    verifica();
+    }
+
+}
 Producto::Producto(void){
 Nombre="";
 numero=0;
 precio=0.0;
+    verifica();
 }
 Producto::Producto(string N, int n, float p){
 this->Nombre=N;
 this->numero=n;
 this->precio=p;
+    verifica();
 }
 void Producto::pideDatos(void){
 cout<<"Nombre del producto:";getline(cin,Nombre);
 cout<<endl<<"Numero de producto:";cin>>numero;
 cout<<endl<<"Precio del producto:";cin>>precio;
+    verifica();
 }
 void Producto::muestraDatos(void){
 cout<<"--------------"<<endl
@@ -33,20 +58,24 @@ return this->numero;
 }
 void Producto::modificaNumero(int n){
 this->numero=n;
+    verifica();
 }
 float Producto::damePrecio(void){
 return this->precio;
 }
 void Producto::modificaPrecio(float p){
 this->precio=p;
+    verifica();
 }
 void Producto::modificaDatos(string N, int n, float p){
 this->Nombre=N;
 this->numero=n;
 this->precio=p;
+    verifica();
 }
 istream& operator>>(istream& teclado, Producto& X){
     X.pideDatos();
+    verifica();
     return teclado;
 }
 ostream& operator<<(ostream& monitor, Producto X){
@@ -87,9 +116,11 @@ ifstream& operator>>(ifstream& DesdeArchivo, Producto& Derecho){
     Derecho.modificaNombre(atributo);
     getline(ss,atributo,',');
     numero = atoi(atributo.c_str());
+    verifica();
     Derecho.modificaNumero(numero);
     getline(ss,atributo);
     precio = atof(atributo.c_str());
+    verifica();
     Derecho.modificaPrecio(precio);
     return DesdeArchivo;
 }
