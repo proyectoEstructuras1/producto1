3e54cf3e77e6e7efcf687599732b559e13cf39a5
proyectoEstructuras1 <57818462+proyectoEstructuras1@users.noreply.github.com>
2019-11-25 19:57:57 -0600

    Add files via upload
    main lista

--- main.cpp
+++ main.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
+#include<string>
 #include<stdlib.h>
 #include"Producto.h"
-#include"ArbolP.h"
+#include"listaP.h"
 using namespace std;
 
-bool menu(ArbolP& a);
-void operaciones(ArbolP& a, int op);
+bool menu(listaP& a);
+void operaciones(listaP& a, int op);
 
 int main(void)
-{ArbolP q;
+{listaP q;
+  /*Producto a("leche",3,12.12), b ("agua",1,12.1),c("ref",2,14.4),d("ref",4,14.4),e("ref",10,14.4),f("ref",9,14.4),g("ref",7,14.4),h("ref",3,14.4);
 
+q.insertaNodo(a);
+q.insertaNodo(b);
+q.insertaNodo(e);
+q.insertaNodo(c);
+q.insertaNodo(d);
+q.insertaNodo(f);
+q.insertaNodo(g);
+q.insertaNodo(h);
+q.eliminaNodo(3);
+q.muestraIniFin();*/
 while(menu(q));
 
     return 0;
 }
-bool menu(ArbolP& a)
+bool menu(listaP& a)
 {
    int op;
    system ("cls");
 
    cout<<"1.- Inserta un producto"<<endl
-       <<"2.- Muestra la lista en pre orden"<<endl
-       <<"3.- Muestra la lista en in orden"<<endl
-       <<"4.- Muestra la lista en pos orden"<<endl
-       <<"5.- Busca un producto"<<endl
-       <<"6.- Nodos en el arbol"<<endl
-       <<"7.- Profundidad del arbol"<<endl
-       <<"8.- Elimina sub arbol"<<endl
-       <<"9.- Elimina un producto"<<endl
-       <<"10.- Elimina el arbol "<<endl
-       <<"11.- Salir"<<endl
+       <<"2.- Muestra la lista"<<endl
+       <<"3.- Busca un producto"<<endl
+       <<"4.- Elimina un producto"<<endl
+       <<"5.- Elimina la lista"<<endl
+       <<"6.- Salir"<<endl
        <<"Cual es tu opcion?";
        cin>>op;
        string enter;
@@ -37,60 +44,39 @@ bool menu(ArbolP& a)
        system("cls");
        operaciones(a,op);
 
-    return op!=11;
+    return op!=6;
 
 }
-void operaciones(ArbolP& a, int op)
+void operaciones(listaP& a, int op)
 {
     Producto p;
 switch(op)
 {
         case 1:
+
             cout<<"Inserta producto:"<<endl;
+
             p.pideDatos();
             a.insertaNodo(p);
            break;
         case 2:
-            cout<<"Esta es el arbol en pre orden"<<endl;
-            a.muestraPre();
+            cout<<"Esta es la lista:"<<endl;
+            a.muestraIniFin();
             break;
         case 3:
-            cout<<"Esta es el arbol en in orden"<<endl;
-            a.muestraIn();
+            a.buscaNodo();
             break;
         case 4:
-            cout<<"Esta es el arbol en pos orden"<<endl;
-            a.muestraPos();
-            break;
-        case 5:
-            int d;
-            cout<<"numero de producto:";cin>>d;
-            a.buscaNodo(d);
-            break;
-        case 6:
-            cout<<"Estos son los nodos en el arbol: ";
-            cout<<a.cuentaNodos()<<endl;
-            break;
-        case 7:
-            cout<<"Esta es la profundidad del arbol: ";
-            cout<<a.profundidadDelArbol()<<endl;
-            break;
-        case 8:
-            int s;
-            cout<<"apartir de que dato desea eliminar:";cin>>s;
-            a.eliminaSubarbol(s);
-            break;
-        case 9:
             int b;
             cout<<"Que numero de producto desea eliminar:";
             cin>>b;
             a.eliminaNodo(b);
             cout<<"Producto eliminado..."<<endl;
             break;
-        case 10:
-            a.eliminaArbol();
+        case 5:
+            a.eliminaLista();
             break;
-        case 11:
+        case 6:
          cout<<"Adios! :v"<<endl<<endl;
             break;
         default:
